7cae89e8b5366e1aaee8030c52419b27055f26d4
Stephen <stephencweiss@gmail.com>
2020-03-04 08:24:24 -0600

    feat: add confirmation page

---
+++ src/pages/Confirm.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Confirm (props) {
+  return (<div>You're on the Confirm page - if you see this it should be because you signed up! We'll be adding some logic shortly to verify that</div>)
+}
