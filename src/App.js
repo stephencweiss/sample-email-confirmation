34449c950ee2ab6d167d77e204bf0c3ab0f2a1a0
Stephen <stephencweiss@gmail.com>
2020-03-04 08:30:32 -0600

    feat: preliminary routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import "./App.css";
+
+import Notifications from "react-notify-toast";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Confirm, Landing } from "./pages";
+import { Loader } from "./components";
+import { API_URL } from "./config";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch(`${API_URL}/wake-up`)
+      .then(res => res.json())
+      .then(() => setIsLoading(false))
+      .catch(err => console.log(err));
+  }, []);
+
+  const Content = () => {
+    if (isLoading) return <Loader />;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/confirm/:id" component={Confirm} />
+          <Route exact path="/" component={Landing} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container fadein">
+      <Notifications />
+      <main>
+        <Content />
+      </main>
+      <footer>Made with love</footer>
     </div>
   );
 }
