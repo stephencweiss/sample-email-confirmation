6a103ffce77214c564bac88853a58bfe1a3c2c30
Stephen <stephencweiss@gmail.com>
2020-03-04 09:01:56 -0600

    feat(wip): add basic form
    still need to add api fetch call

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { notify } from "react-notify-toast";
+import { API_URL } from "../config";
+import { Loader } from "../components";
 
-export function Landing (props) {
-  return (<div>You're on the Landing page - we'll add a form here in a minute for you to confirm your email</div>)
+export function Landing(props) {
+  const [sendingEmail, setSendingEmail] = useState(false);
+  const formRef = useRef();
+  const emailRef = useRef();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSendingEmail(true);
+    try {
+      // send a POST to /email
+      // await response - if it's valid, we're good
+      // toast a success
+      notify.show('success!', 'success')
+    } catch (error) {
+      // if the server sends an error
+      // toast an error
+      notify.show('oh no!!', 'error')
+    } finally {
+      // finally reset form for another submission
+      setTimeout(() =>
+      {formRef.current.reset()
+      setSendingEmail(false)}, 2000);
+    }
+  };
+
+  return (
+    <>
+      <h1>
+        Welcome! Sign up for our amazing service (implementation coming soon)
+      </h1>
+      <form ref={formRef}>
+        <>
+          <input name="email" type="email" ref={emailRef} required />
+          <label htmlFor="email">Email</label>
+        </>
+        <>
+          <button type="submit" onClick={handleSubmit}>
+            {sendingEmail ? <Loader /> : "Submit"}
+          </button>
+        </>
+      </form>
+    </>
+  );
 }
