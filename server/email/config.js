1632bdac5eddda6a78e7deddec4afba26e087a51
Stephen <stephencweiss@gmail.com>
2020-03-07 15:07:48 -0600

    test: replace actual configuration with test
    This change replaces gmail which had authentication concerns.
    Using ethreal allows testing that emails are sending and build out
    remaining flow.
    
    Before publishing, will need to revisit. Issue may have been
    ```diff
    -  secure: true,
    +  secure: false
    ```
    
    Otherwise, it's a GMail thing - which nodemailer confirms is not the
    preferred mail deliverer. For a test app, however, it might be the right
    choice.

--- server/email/config.js
+++ server/email/config.js
@@ -1,28 +1,39 @@
 const nodemailer = require("nodemailer");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// console.log({env: process.env})
 
-const credentials = {
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PW
-  }
-};
-
-const transporter = nodemailer.createTransport(credentials);
+let testAccount;
+let transporter;
+async function createTransport() {
+  testAccount = await nodemailer.createTestAccount();
+  const credentials = {
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user, //process.env.MAIL_USER,
+      pass: testAccount.pass // process.env.MAIL_PW
+    }
+  };
+  transporter = nodemailer.createTransport(credentials);
+}
+createTransport();
 
 async function sendEmail(to, content) {
   const contacts = {
-    from: process.env.MAIL_USER,
+    from: testAccount.user, //process.env.MAIL_USER,
     to
   };
   const email = { ...contacts, ...content };
   try {
-    await transporter.sendMail(email);
+    let emailInfo = await transporter.sendMail(email);
+    console.log(`Email successfully caught by Ethreal.\n Info -> `, emailInfo);
+    console.log(`Preview URL -> `, nodemailer.getTestMessageUrl(emailInfo));
   } catch (error) {
     console.error(`Error sending email ->`, error);
+    throw new Error(error);
   }
 }
 
-module.exports = { send: sendEmail };
+module.exports = sendEmail;
